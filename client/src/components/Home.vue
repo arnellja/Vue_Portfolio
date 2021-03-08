f238cfb6a9088ae44d55c026e34776872c40ffac
Jeffrey Arnell <arnellj@byu.edu>
2021-03-08 16:40:53 -0700

    Start of Vue Portfolio

---
+++ client/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <h2>Welcome to Jeffrey Arnell's Portfolio</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+
+    },
+    created(){
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
